390c460edb205b91cb6f7a03aa3d1452ca3f2fcf
santoshjoseph-toast <santosh.joseph@toasttab.com>
2023-06-21 20:40:09 -0700

    remove ts-ignore

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ import LegandCounter from './components/Legend/LegendCounter';
 import LegandCounterGranular from './components/Legend/LegandCounterGranular';
 import LegendStateContext from './components/StateContext';
 import MultipleCounters from './components/React/MultipleCounters';
-import {State} from './components/State';
 import LegandWithContext from './components/Legend/LegendWithContext';
 
-// @ts-ignore
-const legendState = observable<State>({ count: 0, inc: () => legendState.count.set(prev => prev + 1) })
+const legendState = observable({ count: 0, inc: () => legendState.count.set(prev => prev + 1) })
 
 function App() {
   return (
--- src/components/StateContext.ts
+++ src/components/StateContext.ts
@@ -2,8 +2,7 @@ import { createContext} from 'react';
 import { observable } from "@legendapp/state"
 import {State} from './State';
 
-// @ts-ignore
-const state = observable<State>({ count: 0, inc: () => state.count(prev => prev + 1) })
+const state = observable<State>()
 const StateContext = createContext(state);
 
 export default StateContext;
